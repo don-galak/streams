d1161191645aa0195c979a8f965bfcc4662b4897
Panagiotis Tagkalakis <panagiotis.tagkalakis@eurodyn.com>
2021-04-03 11:10:42 +0300

    show stream screen

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import { Router, Route } from 'react-router-dom'
+import { Router, Route, Switch } from 'react-router-dom'
 import StreamCreate from './streams/StreamCreate'
 import StreamEdit from './streams/StreamEdit'
 import StreamDelete from './streams/StreamDelete'
 import StreamList from './streams/StreamList'
-import { StreamShow } from './streams/StreamShow'
+import StreamShow from './streams/StreamShow'
 import { Header } from './Header';
 import history from '../history'
 
@@ -14,11 +14,13 @@ export const App = () => {
       <Router history={history}>
         <div>
           <Header />
-          <Route path="/" exact component={StreamList}></Route>
-          <Route path="/streams/new" exact component={StreamCreate}></Route>
-          <Route path="/streams/edit/:id" exact component={StreamEdit}></Route>
-          <Route path="/streams/delete/:id" exact component={StreamDelete}></Route>
-          <Route path="/streams/show" exact component={StreamShow}></Route>
+          <Switch>
+            <Route path="/" exact component={StreamList}></Route>
+            <Route path="/streams/new" exact component={StreamCreate}></Route>
+            <Route path="/streams/edit/:id" exact component={StreamEdit}></Route>
+            <Route path="/streams/delete/:id" exact component={StreamDelete}></Route>
+            <Route path="/streams/:id" exact component={StreamShow}></Route>
+          </Switch>
         </div>
       </Router>
     </div>
